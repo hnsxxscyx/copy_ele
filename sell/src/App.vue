77d2c2a2b1a32ed7bcafd1a6759eeda3a8fbccfb
不得不错 <hnsxxscyx@gmail.com>
2017-09-09 21:37:42 +0800

    完成整个APP的骨架设计

---
+++ sell/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <v-header :seller='seller'></v-header>
+    <ul class='tab'>
+        <li><router-link to='/goods'>商品</router-link></li>
+        <li><router-link to='/ratings'>评价</router-link></li>
+        <li><router-link to='/seller'>商家</router-link></li>
+    </ul>
+    <router-view>
+    </router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript6">
+    import header from './components/header/header'
+    import content from './components/content/content'
+    import axios from 'axios'
+
+  
+    export default{
+      data(){
+        return {seller:{}}
+      },
+      created() {
+        // this指向vue实例
+        var that = this
+        axios.get('../static/data.json').then(function (response){
+          that.seller = response.data.seller
+          console.log(this.seller)
+        }).catch(function (error){
+          console.log(error)
+        })
+      },
+      components: {
+        'v-header':header,
+        'v-content':content
+      }
+    }
+</script>
+
+<style lang="scss" scope>
+  @import url('./common/css/icon.scss');
+  .tab {
+        list-style: none;
+        display: flex;
+        line-height: (80rem/75);
+        font-size: (28rem/75);
+        li {
+            flex-grow: 1;
+            text-align: center;
+            a{
+              text-decoration: none;
+              display:inline-block;
+              width: 100%;
+            }
+        }
+    }
+  .router-link-exact-active{
+    color: rgb(240, 20, 20)
+  }
+</style>
